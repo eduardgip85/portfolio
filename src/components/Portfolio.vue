26a83100da801ee2591eb1c13e767b0b20784414
Eduard <eduardgip85@gmail.com>
2021-03-13 19:03:06 +0100

    Portfolio

--- src/components/Portfolio.vue
+++ src/components/Portfolio.vue
@@ -9,17 +9,31 @@
                 </div>
             </div>
             <div class="row">
-                <b-card-group deck class="p-2"> 
+                <b-card-group deck class="p-2" style="max-width: 100%"> 
                     <b-card style="max-width: 400px" class="text-center">
                         
                         <b-col md="12">
                             <a href="">
-                                <img src="../assets/background.jpg" alt="" style="max-width:400px; height: auto; max-height: 170px">
+                                <img src="../assets/background.jpg" alt="" style="max-width:400px; height: auto; max-height: 170px; width: 100%">
                             </a>
                         </b-col>
 
-                        <b-card-title>{{this.portfolio[0].titulo}}</b-card-title>
+                        <b-card-title class="pt-2">{{this.portfolio[0].titulo}}</b-card-title>
                         <b-card-text>{{this.portfolio[0].desc}}</b-card-text>
+                        <b-card-sub-title>Tecnologias usadas</b-card-sub-title>
+                        
+                        <b-row style="justify-content: center" class="p-2">
+                            <div v-for="(item, index) in this.portfolio[0].tecnologias" :key="index" >
+                                
+                                {{item}}
+                                
+                                <span v-if="index < item.length - 1" style="float: right" class="pl-2 pr-2"> 
+                                 |
+                                </span>
+
+                            </div>
+                        </b-row>
+                
 
                         <b-button href="#" variant="primary">Go somewhere</b-button>
                     </b-card>
@@ -43,14 +57,14 @@ export default {
                     img:'../assets/background.jpg',
                     titulo: 'AMARTEC',
                     desc: 'Desarrollo de pagina web corporativa desde 0 para la empresa de gestion de espacios amartec',
-                    tecnologias: '',
+                    tecnologias: ['Laravel','Html','CSS'],
                     link: ''
                 },
                 {
                     img: '../assets/logo.png',
                     titulo: 'Proj 2',
                     desc: '',
-                    tecnologias: '',
+                    tecnologias: [''],
                     link: ''
                 }
             ],
